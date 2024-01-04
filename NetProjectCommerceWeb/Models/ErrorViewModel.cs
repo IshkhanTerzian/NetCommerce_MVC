08d88c080f664fe30180140fd68d919c182c80a2
Ishkhan <ioterzian@gmail.com>
2024-01-04 13:13:52 -0500

    Add project files.

---
+++ NetProjectCommerceWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetProjectCommerceWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
